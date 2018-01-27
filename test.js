bebebee11cfb824b59128ac25acfdbcbfa66ea62
ramsai4243 <ram.sai4243@gmail.com>
2018-01-26 21:03:24 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,28 @@
+function disp(){
+var a=document.getElementById("virat").value;
+document.getElementById("virat1").innerHTML="a";
+var b=document.getElementById("dhoni").value;
+document.getElementById("dhoni1").innerHTML="b";
+var c=document.getElementById("beckham").value;
+document.getElementById("beckham1").innerHTML="c";
+var d=document.getElementById("roony").value;
+document.getElementById("roony1").innerHTML="d";
+var e=document.getElementById("v1").value;
+document.getElementById("v2").innerHTML="e";
+var g=document.getElementById("d1").value;
+document.getElementById("d2").innerHTML="g";
+var i=document.getElementById("bk1").value;
+document.getElementById("bk2").innerHTML="i";
+var k=document.getElementById("ro1").value;
+document.getElementById("ro2").innerHTML="k";
+}
+function disp2(){
+var f=document.getElementById("enter").value;
+document.getElementById("demo").innerHTML="f";
+var h=document.getElementById("dh1").value;
+document.getElementById("dh2").innerHTML="h";
+var j=document.getElementById("bk3").value;
+document.getElementById("bk4").innerHTML="j";
+var l=document.getElementById("ro3").value;
+document.getElementById("ro4").innerHTML="l";
+}
